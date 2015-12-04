b0e61208af8fd1b518b9c57462b177cfa26be387
Búi Eymundsson <Bi Eymundsson>
2015-12-04 13:17:36 +0000

    Bætti ID við person klasann

--- sqlite.cpp
+++ sqlite.cpp
@@ -8,7 +8,7 @@ SQLite::SQLite()
     getData();
 }
 
-void SQLite::getData()       //Sækir gögn úr skrá og geymir í vektor.
+void SQLite::getData()       //Sækir gögn úr gagnagrunni og geymir í vektor.
 {
     m_db = QSqlDatabase::addDatabase("QSQLITE");
     QString dbName = "prufa.sqlite";
@@ -24,27 +24,23 @@ void SQLite::getData()       //Sækir gögn úr skrá og geymir í vektor.
     while(query.next())
     {
 
-        //int id = query.value("id").toUInt();
+        int id = query.value("id").toUInt();
         string name = query.value("name").toString().toStdString();
         string gender = query.value("gender").toString().toStdString();
         int dob = query.value("yearOfBirth").toUInt();
         int dod  = query.value("yearOfDeath").toUInt();
 
-        Person newguy(name, gender, dob, dod);
+        Person newguy(id, name, gender, dob, dod);
         m_personList.push_back(newguy);
     }
 }
 
-void SQLite::saveData()                      //Sækir gögn úr vektor og vistar í skrá.
+void SQLite::saveData()                       //Sækir gögn úr vektor og vistar í skrá.
 {
     m_db.close();
 }
 
-/******************************************************************************
- * Fall:   addData
- * Lýsing: Bætir við persónu í gagnagrunn.
- *****************************************************************************/
-void SQLite::addData(Person& p)
+void SQLite::addData(Person& p)               // Vistar persónu í gagnagrunninn.
 {
     const QString sInsertSQL = QString("Insert into person(name, gender, yearOfBirth, yearOfDeath) values ('%1','%2',%3,%4)").arg(QString::fromStdString(p.getName()),QString::fromStdString(p.getGender()),QString::number( p.getDayOfBirth()),QString::number(p.getDayOfDeath()));
 
